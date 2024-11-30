7078149626a436e7cbd98ef8c2417e91883d6899
salles4 <fjsalles.04@gmail.com>
2024-11-30 23:03:41 +0800

    fix cors error

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 require("dotenv").config()
 
 const express = require("express")
-const app = express();
+const cors = require('cors')
 
+const app = express();
+app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.listen(8000, () => {
